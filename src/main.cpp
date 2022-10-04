7031c8cb7922b32a56ff647aa6c6c24b10f0afee
PepitoLeFameux <123roman35@gmail.com>
2022-10-04 14:47:17 +0200

    Creation main.cpp + map.png

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include "raylib.h"
+
+int main(int argc, char const *argv[])
+{
+    const int largeur = 1920;
+    const int hauteur = 1080;
+
+    Camera3D camera = Camera();
+    camera.position = {0, 0, 0};
+    
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
