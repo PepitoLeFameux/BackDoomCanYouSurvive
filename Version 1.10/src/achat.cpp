6c97ff36fede9bc8ab44c4f97f14a7869da02e99
plrdmael <mael.plard@esme.fr>
2022-11-15 17:00:39 +0100

    collision + sons + miniMap + menus

---
+++ Version 1.10/src/achat.cpp
@@ -0,0 +1,67 @@
+#include "achat.h"
+#include "raylib.h"
+#include "raymath.h"
+#include "arme.h"
+#include "iostream"
+
+
+void Achat::Init(Camera *camera, Arme *arme, bool *unlocked, int *argent){
+
+    Achat::argent = argent ;
+    Achat::camera = camera ; 
+    Achat::unlocked = unlocked ; 
+    Achat::arme = arme ; 
+    for (int num = 0 ; num <= 5 ; num++){
+        std::string strTexture = "../resources/ath/weapon" + std::to_string(num + 1) + ".png" ;
+        weapon[num] = LoadTexture(strTexture.c_str());
+        rec_src[num] = {0,0,(weapon[num]).width, (weapon[num]).height};
+    }
+
+    cashRegister = LoadSound("../resources/sound/cashRegister.wav");
+
+}
+
+
+void Achat::Render(){
+
+    DrawBillboardRec(*camera, weapon[0], rec_src[0], positionAchatListe[0], (Vector2){0.25f,0.20f}, WHITE);
+    DrawBillboardRec(*camera, weapon[2], rec_src[2], positionAchatListe[2], (Vector2){0.25f,0.20f}, WHITE);
+    DrawBillboardRec(*camera, weapon[4], rec_src[4], positionAchatListe[4], (Vector2){0.3f,0.20f}, WHITE);
+    DrawBillboardRec(*camera, weapon[5], rec_src[5], positionAchatListe[5], (Vector2){0.2f,0.20f}, WHITE);
+
+}
+
+void Achat::RenderMessage(){
+
+    for (int n=0; n<=5; n++){
+        if(achatPossibleListe[n] == true){
+            std::string strAchat = "Appuyez sur B pour acheter l'arme (" + std::to_string(prixListe[n]) +" $)";
+            if(*argent <= prixListe[n]) DrawText(strAchat.c_str(), 600,950,40,RED);
+            if(*argent >= prixListe[n]) DrawText(strAchat.c_str(), 600,950,40, LIME);
+        }
+    }
+}
+
+void Achat::Acheter(){
+
+    Vector3 cameraPos = (*camera).position;
+    
+    for (int n = 0 ; n <= 5 ; n++){
+        distJoueurListe[n] = sqrt(pow(positionAchatListe[n].x - cameraPos.x, 2) + pow(positionAchatListe[n].z - cameraPos.z, 2));
+        if(distJoueurListe[n] <= 0.2f) achatPossibleListe[n] = true ; 
+        else achatPossibleListe[n] = false ; 
+
+        if(achatPossibleListe[n] == true){
+            if(IsKeyPressed(KEY_B) && *argent >= prixListe[n] && unlocked[n+1] == false){
+                *argent -= prixListe[n] ; 
+                PlaySound(cashRegister);
+                unlocked[n+1] = true ;
+                unlocked[arme->numeroArme] = false ;
+                arme->numeroArme = n+1 ;  
+            }
+        }
+    }
+
+
+
+}
---
+++ Version 1.10/src/achat.h
@@ -0,0 +1,37 @@
+#include "raylib.h"
+#include "arme.h"
+
+#ifndef ACHAT_H
+#define ACHAT_H
+
+class Achat
+{
+    public:
+
+    bool *unlocked ; 
+    int distJoueurListe[6] ;
+
+    Arme *arme ;
+    Camera *camera ;
+    Sound cashRegister ; 
+
+    Texture2D weapon[6] ;
+    Rectangle rec_src[6] ; 
+    bool achatPossibleListe[6] = {false, false, false, false, false, false};
+    Vector3 positionAchatListe[6] = {{1.0f,0.3f,1.0f},{},{0.0f,0.3f,5.0f},{},{0.0f,0.3f,3.0f},{2.0f,0.3f,3.0f}};
+    int prixListe[6] = {1500,100,750,3500,5000,7000} ; 
+
+    void Init(Camera *camera, Arme *arme, bool *unlocked, int *argent);
+    void Render();
+    void RenderMessage();
+    void Acheter();
+
+    private :
+    int *argent ;
+};
+
+
+
+
+
+#endif
